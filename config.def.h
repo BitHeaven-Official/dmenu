c1d0361d24d0b7d5c1d962345752901058ec3727
BitHeaven-Official <o.o@bitheaven.ru>
2023-03-08 13:53:26 +0500

    Fix changes

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -21,6 +21,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-static unsigned int borderwidth = 3;
+static unsigned int borderwidth = 5;
 static unsigned int bordervisible = 0;
-static const unsigned int gappx = 10;
+static const unsigned int gappx = 15;
